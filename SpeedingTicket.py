c7c93f6030aa256a48d765cf5b3b45fbc2a884f8
mirkymoo <vlada.cherkasskaya@gmail.com>
2023-09-25 14:52:12 -0400

    LogicError bug fixes

--- SpeedingTicket.py
+++ SpeedingTicket.py
@@ -3,12 +3,12 @@ if speed_limit <= 0:
         print("Invalid input for the speed limit. Please make sure you are entering a positive number greater than 0.")
 elif speed_limit > 0:
         driving_speed = int(input("Enter how fast you are driving: \n"))
-        if driving_speed <= 0:
-                print("Invalid input for the driving speed. Please make sure you are entering a positive number greater than 0.")
-        elif driving_speed > 0:
-                if driving_speed - speed_limit == 0:
+        if driving_speed < 0:
+                print("Invalid input for the driving speed. Please make sure you are entering a positive number.")
+        elif driving_speed >= 0:
+                if driving_speed == 0:
                         print("You are not driving! You're car is parked.")
-                elif speed_limit - 10 < driving_speed < speed_limit:
+                elif driving_speed < speed_limit - 10:
                          print("You are driving too slow! You will get a $50 traffic ticket.")
                 elif speed_limit + 6 <= driving_speed <= speed_limit +20:
                          print("You are driving too fast! You will get a $75 speeding ticket.")
