8ab41be7fca6d8282f3b122802541697a71f4773
mirkymoo <vlada.cherkasskaya@gmail.com>
2023-09-25 14:35:06 -0400

    Checks input for positive value

--- SpeedingTicket.py
+++ SpeedingTicket.py
@@ -1,13 +1,20 @@
 speed_limit = int(input("Enter the speed limit: \n"))
-driving_speed = int(input("Enter how fast you are driving: \n"))
-
-if driving_speed <= speed_limit - 10:
-    print("You are driving too slow! You will get a $50 traffic ticket.")
-elif speed_limit + 6 <= driving_speed <= speed_limit +20:
-        print("You are driving too fast! You will get a $75 speeding ticket.")
-elif speed_limit + 21 <= driving_speed <= speed_limit + 40:
-        print("You are driving too fast! You will get a $150 speeding ticket.")
-elif driving_speed > speed_limit + 40:
-        print("You are driving too fast! You will get a $300 speeding ticket.")
-else:
-    print("You are driving at an appropriate speed. You will not be issued a traffic ticket.")
+if speed_limit <= 0:
+        print("Invalid input for the speed limit. Please make sure you are entering a positive number greater than 0.")
+elif speed_limit > 0:
+        driving_speed = int(input("Enter how fast you are driving: \n"))
+        if driving_speed <= 0:
+                print("Invalid input for the driving speed. Please make sure you are entering a positive number greater than 0.")
+        elif driving_speed > 0:
+                if driving_speed - speed_limit == 0:
+                        print("You are not driving! You're car is parked.")
+                elif speed_limit - 10 < driving_speed < speed_limit:
+                         print("You are driving too slow! You will get a $50 traffic ticket.")
+                elif speed_limit + 6 <= driving_speed <= speed_limit +20:
+                         print("You are driving too fast! You will get a $75 speeding ticket.")
+                elif speed_limit + 21 <= driving_speed <= speed_limit + 40:
+                        print("You are driving too fast! You will get a $150 speeding ticket.")
+                elif driving_speed > speed_limit + 40:
+                        print("You are driving too fast! You will get a $300 speeding ticket.")
+                else:
+                        print("You are driving at an appropriate speed. You will not be issued a traffic ticket.")
