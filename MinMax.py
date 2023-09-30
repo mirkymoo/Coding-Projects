6dc2f20ff888c9f681c296816e6012e30416e58c
mirkymoo <vlada.cherkasskaya@gmail.com>
2023-09-29 20:12:53 -0400

    added code to find biggest/smallest value

---
+++ MinMax.py
@@ -0,0 +1,22 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+print("The smallest number is:", min(num1, num2, num3))
+print("The largest number is:", max(num1, num2, num3))
+
+
+# using only if/esle statments
+if num1 < num2 and num1 < num3:
+    print(num1, "is the smallest number")
+elif num2 < num1 and num2 < num3:
+    print(num2, "is the smallest number")
+else:
+    print(num3, "is the smallest number")
+
+if num1 > num2 and num1 > num3:
+    print(num1, "is the largest number")
+elif num2 > num1 and num2 > num3:
+    print(num2, "is the largest number")
+else:
+    print(num3, "is the largest number")
