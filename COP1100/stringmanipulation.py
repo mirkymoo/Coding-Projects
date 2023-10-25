8091a97674504f0f17b978aef3b889968eafc02c
mirkymoo <vlada.cherkasskaya@gmail.com>
2023-10-25 09:24:11 -0400

    way to manipulate strings

---
+++ COP1100/stringmanipulation.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Your name is:", first_name + " " + last_name)
+
+
+message = input("Enter a message to print three times: ")
+print((message + " ") * 3)
