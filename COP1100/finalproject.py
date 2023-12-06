8ac199d49e79d9d2a7fa4fd8e7f5c500fd22eefe
mirkymoo <vlada.cherkasskaya@gmail.com>
2023-12-06 00:44:13 -0500

    added new functions

--- COP1100/finalproject.py
+++ COP1100/finalproject.py
@@ -12,30 +12,22 @@ def validate_input(prompt):
         except ValueError:
             print("Invalid input. Please enter a numeric value.")
 
-services_total = 0
+def getChoiceSelection():
+    selection = int(input("Enter choice: "))
+    return(selection)
 
-def mainMenu():
-    print("\nPlease select an option:")
+def getRoomCharge():
+    days = int(input("Enter how many days did the patient stay at the hospital:"))
+    return(days * 1662)
+
+def getPatientType():
+    print("\nMain Menu")
+    print("\nIs this an in-patient or out-patient?")
     print("1. In-Patient")
     print("2. Out-Patient")
     print("3. Quit")
-    while True:
-        try:
-            selection = int(input("Enter choice: "))
-            if selection == 1:
-                inPatientServicesMenu()
-                break
-            elif selection == 2:
-                outPatientServicesMenu()
-                break
-            elif selection == 3:
-                break
-            else:
-                print("Invalid choice. Enter 1-3")
-                mainMenu()
-        except ValueError:
-            print("Invalid choice. Enter 1-3")
-    exit
+    patientType = int(input("Enter choice: "))
+    return(patientType)
 
 def inPatientServicesMenu():
     print("\nIn-Patient Services Menu:")
@@ -49,19 +41,30 @@ def inPatientServicesMenu():
     print("7. POST PARTUM TUBAL - $2336")
     print("8. FETAL VERSION - $3828")
     print("9. Return to main menu.")
-    prices = [2924, 4200, 3200, 6200, 5200, 4341, 2336, 3828]
     while True:
         try:
-            selection = int(input("Enter choice: "))
+            selection = getChoiceSelection()
             if (selection > 0 and selection < 9):
-                services_total += selection
+                getServiceCharge(selection)
             elif selection == 9:
-                mainMenu()
+                getPatientType()
             else:
-                print("Invalid choice. Enter 1-9")
+                print("Invalid choice. Enter 1-6")
                 inPatientServicesMenu()
         except ValueError:
-            print("Invalid choice. Enter 1-9")
+            print("Invalid choice. Enter 1-6")
+
+def getServiceCharge(selection):
+    patientType = getPatientType()
+    if patientType == 1:
+        prices = {1: 2924, 2: 4200, 3: 3200, 4: 6200, 5: 5200, 6: 4341, 7: 2336, 8: 3828}
+        serviceCharge = prices[selection]
+    elif patientType == 2:
+        prices = {1: 96, 2: 106, 3: 900, 4: 450, 5: 506}
+        serviceCharge = prices[selection]
+    else:
+        print("Error in getServiceCharge")
+    return(serviceCharge)
 
 def outPatientServicesMenu():
     print("\nOut-Patient Services Menu:")
@@ -71,14 +74,13 @@ def outPatientServicesMenu():
     print("4. US OB COMPL EX ADDTL GESTATN- $450")
     print("5. US OB LIMITED 1+FETUSES - $506")
     print("6. Return to main menu.")
-    prices = [96, 106, 900, 450, 506]
     while True:
         try:
-            selection = int(input("Enter choice: "))
+            selection = getChoiceSelection()
             if (selection > 0 and selection < 6):
-                services_total += prices[selection - 1]
+                getServiceCharge(selection)
             elif selection == 6:
-                mainMenu()
+                getPatientType()
             else:
                 print("Invalid choice. Enter 1-6")
                 outPatientServicesMenu()
@@ -93,18 +95,51 @@ def medicationMenu():
     print("4. OB OR ANESTHESIA EA ADDL 15 MIN - $450")
     print("5. US OB LIMITED 1+FETUSES - $506")
     print("6. Return to main menu.")
-    prices = [669, 472, 900, 450, 506]
     while True:
         try:
-            selection = int(input("Enter choice: "))
+            selection = getChoiceSelection()
             if (selection > 0 and selection < 6):
-                services_total += prices[selection - 1]
+                getMedicationCharge(selection)
             elif selection == 6:
-                mainMenu()
+                getPatientType()
             else:
                 print("Invalid choice. Enter 1-6")
                 medicationMenu()
         except ValueError:
             print("Invalid choice. Enter 1-6")
 
-mainMenu()
+def getMedicationCharge(selection):
+    prices = {1: 669, 2: 472, 3: 900, 4: 450, 5: 506}
+    medicationCharge = prices[selection]
+    return(medicationCharge)
+
+def main():
+    patientType = getPatientType()
+    while True:
+        try:
+            if patientType == 1:
+                roomCharge = getRoomCharge()
+                inPatientServicesMenu()
+                break
+            elif patientType == 2:
+                outPatientServicesMenu()
+                break
+            elif patientType == 3:
+                break
+            else:
+                print("Invalid choice. Enter 1-3")
+                getPatientType()
+        except ValueError:
+            print("Invalid choice. Enter 1-3")
+    
+    medicationCharge = getMedicationCharge()
+    serviceCharge = getServiceCharge()
+
+    totalCharge = roomCharge + serviceCharge + medicationCharge
+    print(f"Room charges \t\t${roomCharge:0.2f}")
+    print(f"Lab & Services \t\t${serviceCharge:0.2f}")
+    print(f"Medication \t\t${medicationCharge:0.2f}")
+    print(f"Total charges \t\t${totalCharge:0.2f}")
+
+main()
+
